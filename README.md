b6efb378e0752ede4441c9d2fac9dec39d83d251
Silvano Gomes do Amaral <silvano.amaral@spcdc.br>
2020-05-28 21:19:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto intellibrand Danone Nutridrink
+
+## Começando
+Essas instruções fornecerão uma cópia do projeto em execução na máquina local para fins de desenvolvimento e teste
+
+### `Instalação do projeto`
+
+* Copie o repositório em sua máquina: https://github.com/silvanoamaral/intellibrand-danone.git
+* Acesse a pasta `cd intellibrand-danone`
+* Instale as dependências no projeto
+
+```bash
+# Instale as dependências
+npm install
+
+# Execute o comando
+npm start
+
+```
+
+## Avaliando o projeto
+
+No diretório do projeto, você pode executar:
+
+### `npm start`
+
+Executa o aplicativo no modo de desenvolvimento. <br />
+Abra [http://localhost:3000](http://localhost:3000) para visualizá-lo no navegador.
---
+++ src/View/FooterViews.js
@@ -0,0 +1,25 @@
+class FooterViews {
+  constructor(elemento) {
+    this._elemento = elemento
+  }
+
+  _template() {
+    return `<div class="disclaimer">
+      <div class="container">
+        <p>Os produtos indicados não contém gluten</p>
+        <p class="margin">Imagens ilustrativas</p>
+        <p>*1.Gombart, Adrian F et al. “A Review of Micronutrients and the Immune System-Working in Harmony to Reduce the Risk of Infection.” Nutrients vol. 12,1 236. 16 Jan. 2020, doi:10.3390/nu12010236</p>
+        <p>2.Barazzoni, Rocco et al. “ESPEN expert statements and practical guidance for nutritional management of individuals with SARS-CoV-2 infection.” Clinical nutrition (Edinburgh, Scotland), S0261-5614(20)30140-0. 31 Mar. 2020, doi:10.1016/j.clnu.2020.03.022</p>
+        <p>3.Alwarawrah, Yazan et al. “Changes in Nutritional Status Impact Immune Cell Metabolism and Function.” Frontiers in immunology vol. 9 1055. 16 May. 2018, doi:10.3389/fimmu.2018.01055</p>
+      </div>
+    </div>
+    <div class="container">
+      <strong>Consulte sua nutricionista e/ou médico</strong>
+      <img src="src/assets/images/imagen-footer.png" alt="imagen-footer" />
+    </div>`
+  }
+
+  update() {
+    this._elemento.innerHTML = this._template()
+  }
+}
