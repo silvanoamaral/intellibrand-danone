46be447a3f8ffa0e4681e33a953b3f11b5734fdd
silvanoamaral <silvano.amaral9@gmail.com>
2020-05-31 19:56:14 -0300

    Update obterDetalhesProd.js

--- src/services/obterDetalhesProd.js
+++ src/services/obterDetalhesProd.js
@@ -2,9 +2,7 @@ const container = document.querySelector('body')
 var eleBody = new LightBoxView(container)
 
 async function obterDetalhesProd(ean) {
- alert('ean', ean)
   var token = sessionStorage.getItem('tokenintellibrand')
-  alert('token', token)
 
   var resPrd = await fetch(`https://locatestore.intellibrand.ai/product/${ean}`, {
     method: 'get',
